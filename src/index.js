f29bf79f5a3e8da673c419337447106d92e4650f
facuriffel <facundoriffel04@gmail.com>
2026-08-12 17:33:46 -0300

    feat: endpoint POST /auth/login con jwt y validacion bcrypt

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-// Middleware para entender los datos que lleguen en formato JSON
 app.use(express.json());
 
-// Importamos y usamos nuestra ruta de prueba
+// rutas
 app.use('/health', require('./routes/health.routes'));
+app.use('/auth', require('./routes/auth.routes'));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor de la clínica corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
